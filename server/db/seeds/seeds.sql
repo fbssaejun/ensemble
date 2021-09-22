07177c05c792e47af564d7a0b357cbf4e12395a2
Anthony Kim <fbssaejun@gmail.com>
2021-09-22 23:06:21 +0000

    database migration and setup

---
+++ server/db/seeds/seeds.sql
@@ -0,0 +1,45 @@
+INSERT INTO users (first_name, last_name, email, username, password, country, city, province, street, post_code)
+VALUES ('First', 'User', 'none@none.com', 'somedude', '1234', 'USA', 'Orlando', 'Florida', '1375 E Buena Vista Dr', '32830');
+
+
+
+INSERT INTO bands (leader_id, name, featured)
+VALUES
+(1, 'Oasis', TRUE),
+(1, 'Coldplay', FALSE),
+(1, 'AC/DC', TRUE),
+(1, 'Red Hot Chili Peppers', TRUE),
+(1, 'Lamb of God', FALSE),
+(1, 'Polyphia', TRUE),
+(1, 'Racer X', TRUE),
+(1, 'Chon', TRUE),
+(1, 'Periphery', FALSE);
+
+
+
+INSERT INTO instruments (name)
+VALUES
+('Guitar'),
+('Bass'),
+('Drums'),
+('Piano'),
+('Violin'),
+('Cello'),
+('Vocal');
+
+
+INSERT INTO genres (name)
+VALUES
+('Classic'),
+('Jazz'),
+('Hiphop'),
+('Rock'),
+('Progressive rock'),
+('Heavy Metal'),
+('Progressive metal'),
+('Djent');
+
+
+
+
+

c8dc670926cfe80c4d2d7f58e7edda3bdaa91338
KangerDrew <kangdrew95@gmail.com>
2021-09-23 13:46:36 -0700

    Fixed the npm installation issue

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8081;
 const db = require("./db/index");
 const router = require("express").Router();
 

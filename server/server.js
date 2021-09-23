97f4fc374ab457dcc906c69776dd1174397e8c5d
Anthony Kim <fbssaejun@gmail.com>
2021-09-23 18:08:32 -0400

    edit path

--- server/server.js
+++ server/server.js
@@ -18,11 +18,6 @@ app.use(bodyParser.json());
 app.use(express.static('public'));
 
 
-// Sample GET route
-app.get('/api/data', (req, res) => res.json({
-  message: "Seems to work!",
-}));
-
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Express seems to be listening on port ${PORT} so that's pretty good 👍`);

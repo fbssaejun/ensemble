178e1a68e5b7e0a7e354fb3fb939c8881a73ac1b
Anthony Kim <fbssaejun@gmail.com>
2021-09-24 22:58:21 -0400

    use history to render page conditionally

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,6 +1,5 @@
 import axios from 'axios';
-import { Link } from 'react-router-dom';
-import axios from 'axios';
+import { Link, useHistory } from 'react-router-dom';
 import { useState } from 'react';
 import { chainPropTypes } from '@mui/utils';
 
@@ -8,6 +7,7 @@ export default function Login(props) {
   // const [state, setState] = useState();
   // const [user, setUser] = useState();
   const [userId, setUserId] = useState();
+  const history = useHistory();
   
   const loginCheck = (event) => {
     event.preventDefault();
@@ -17,22 +17,16 @@ export default function Login(props) {
         console.log("No user")
       } else {
         props.setState((prev) => ({ ...prev, currentUser: response.data[0].id }));
+        history.push('/');
       }
     }).catch(e => console.log(e))
   }
 
-  axios.post()
-
   return(
     <form onSubmit={loginCheck}>
       <div class="form-group">
-<<<<<<< HEAD
-        <input type="text" placeholder="Enter id"/>
-        <button><Link to="/login">Login</Link></button>
-=======
         <input type="text" placeholder="Enter id" onChange={({ target }) => setUserId(target.value)}/>
-        <button type="submit"><Link to="/login">Login</Link></button>
->>>>>>> 1f344b5e3864ee6a3025c341c79766d13b6b1a51
+        <button type="submit"><Link to="/login"></Link>Login</button>
       </div>
     </form>
   );

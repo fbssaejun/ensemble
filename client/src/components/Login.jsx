7b129e772d6c2f09f1b0b5d07097c2b0142aa9d9
Tzheng456 <Tzheng456@gmail.com>
2021-09-24 18:26:10 -0700

    merge conflict

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,12 +1,15 @@
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 
 
 export default function Login(props) {
 
+  axios.post()
+
   return(
     <form>
       <div class="form-group">
-        <input type="text" placeholder="Enter email"/>
+        <input type="text" placeholder="Enter id"/>
         <button><Link to="/login">Login</Link></button>
       </div>
     </form>

363325749ed44ee3ff6be4d5f86b9662e07b8219
Anthony Kim <fbssaejun@gmail.com>
2021-09-27 15:27:11 -0400

    add spot convert function to insert data through post request

--- client/src/components/CreateBandForm.jsx
+++ client/src/components/CreateBandForm.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useHistory } from 'react-router-dom';
-import Spot from './Spot';
+import FormSpot from './FormSpot';
 
 export default function CreateBandForm(props) {
   const [bandName, setBandName] = useState("");
   const [bandDesc, setBandDesc] = useState("");
   const [bandFeatured, setBandFeatured] = useState(false);
   const [bandImage, setBandImage] = useState("");
-  const [spotArr, setSpotArr] = useState([])
+  const [spotArr, setSpotArr] = useState([]);
   const history = useHistory();
 
   const submitForm = (event) => {
@@ -20,23 +20,34 @@ export default function CreateBandForm(props) {
       description: bandDesc,
       band_image: bandImage,
       featured: bandFeatured,
-      spotTitles: []
+      spotData: spotConvert(spotArr)
     })
     .then(response => {
-      const newBandId = response.data.result.id;
+      const newBandId = response.data.result.band_id;
       history.push(`/bands/${newBandId}`)
     }).catch(e => console.log(e))
   }
 
-  const addSpot = () => {
-    setSpotArr((prev) => [...prev, {title:"", username:""}]);
+  const spotConvert = (arr) => {
+    const retObj = {title:[], username: [], instrumentId: [], description: []};
+    arr.forEach((spot) => {
+      retObj.title.push(spot.title);
+      retObj.username.push(spot.username);
+      retObj.instrumentId.push(spot.instrumentId);
+      retObj.description.push(spot.description);
+    })
+    return retObj;
   }
+
+  const addSpot = () => {
+    setSpotArr((prev) => [...prev, {title:"", username:"", instrumentId:"", description:""}]);
+  };
 
   const updateSpot = (index, changedSpot) => {
     const newSpots = [...spotArr]
-    newSpots.splice(index, 1, changedSpot)
+    newSpots.splice(index, 1, changedSpot);
     setSpotArr((prev) => [...newSpots]);
-  }
+  };
 
   const deleteSpot = (index) => {
     const newSpots = [...spotArr];
@@ -46,7 +57,7 @@ export default function CreateBandForm(props) {
 
   return(
     <form onSubmit={submitForm}>
-      <h2>{JSON.stringify(spotArr)}</h2>
+      <h2>{JSON.stringify(spotConvert(spotArr))}</h2>
       <div className="form-group-band">
         <h2>Create a new Band</h2>
         <input type="text" placeholder="Enter name of band" onChange={({ target }) => setBandName(target.value)}/> <br/>
@@ -55,7 +66,7 @@ export default function CreateBandForm(props) {
         <input type="checkbox" checked={bandFeatured} id="featuredCheck" onChange={() => setBandFeatured(!bandFeatured)}/>
         <label for="featuredCheck">Feature Band?</label> <br />
         {spotArr.map((obj, index) => {
-          return <Spot key={index} onDelete={() => {deleteSpot(index)}} onUpdate={updateSpot} index={index} spot={obj} />
+          return <FormSpot key={index} onDelete={() => {deleteSpot(index)}} onUpdate={updateSpot} index={index} spot={obj} />
         })}
         <button type="button" onClick={() => addSpot()}>Add A Spot</button>
         <button type="submit">Create band</button>

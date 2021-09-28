a0b25ba5bc1b74e31cf3380fe6e6229a1a9e556d
Anthony Kim <fbssaejun@gmail.com>
2021-09-27 22:04:03 -0400

    add delete button to created bands

--- client/src/components/FormSpot.jsx
+++ client/src/components/FormSpot.jsx
@@ -16,9 +16,9 @@ export default function FormSpot(props) {
   return(
     <div className="form-group-spot">
       <h2>New Spot</h2>
-      <input type="text" placeholder="Title" onChange={({ target }) => setTitle(target.value)}/> <br/>
-      <input type="text" placeholder="Username" onChange={({ target }) => setUsername(target.value)}/> <br/>
-      <input type="text" placeholder="Instrument" onChange={({ target }) => setInstrumentId(target.value)}/> <br/>
+      <input type="text" placeholder="Title" onChange={({ target }) => setTitle(target.value)} required/> <br/>
+      <input type="text" placeholder="Username" onChange={({ target }) => setUsername(target.value)} required/> <br/>
+      <input type="text" placeholder="Instrument" onChange={({ target }) => setInstrumentId(target.value)} required/> <br/>
       <textarea type="text" placeholder="Description" onChange={({ target }) => setDescription(target.value)}/> <br/>
       <button type="button" onClick={props.onDelete}>Delete</button>
     </div>

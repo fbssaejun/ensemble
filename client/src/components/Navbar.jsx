63f5f9e064901fdf2515a8d3d917bb784c3474c6
Anthony Kim <fbssaejun@gmail.com>
2021-09-27 22:09:37 -0400

    remove console.log

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,18 +1,12 @@
 import React from 'react';
 import './Navbar.scss'
 import { Link, useHistory } from 'react-router-dom';
-import UserProfile from './UserProfile';
-
 
 export default function Navbar(props) {
   const history = useHistory();
   const logOut = () => {
     props.setState(prev => ({...prev, currentUser: undefined}))
     history.push("/");
-  }
-
-  const goToProfile = () => {
-
   }
 
   return (
@@ -23,12 +17,12 @@ export default function Navbar(props) {
             <input type="text" placeholder="Search Band" />
             <button>Search</button>
           </div>
-          {!props.user ? (<div className="user">
+          {!props.currentUser ? (<div className="user">
             <Link to="/login">Login</Link>
             <Link to="/signup">Signup</Link>
           </div>) : (
           <React.Fragment>
-            <Link to={`/users/${props.user}`}> Profile {props.user} </Link>
+            <Link to={`/users/${props.currentUser.id}`}> Profile {props.currentUser.username} </Link>
             <Link to="/bands/new">Create New Band</Link>
             <button type="submit" onClick={logOut}>Logout</button>
           </React.Fragment>

8426ae72336756a653648fbd96de5a16cd4a6fc4
KangerDrew <kangdrew95@gmail.com>
2021-09-27 19:58:58 -0700

    Dropdown added to searchbar

--- client/src/components/Search/index.jsx
+++ client/src/components/Search/index.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react"
 
 
 export default function Search(props) {
@@ -5,7 +6,19 @@ export default function Search(props) {
   return(
     <form>
       <input type="text" placeholder="Find your Band" />
-      <button>Submit</button>
+      <button>Submit</button> <br/>
+      <select name="instruments">
+        <option value="1">Guitar</option>
+        <option value="2">Flute</option>
+        <option value="3">Bass</option>
+        <option value="4">Vocal</option>
+      </select>
+      <select name="genre">
+        <option value="1">Jazz</option>
+        <option value="2">Rock</option>
+        <option value="3">Blues</option>
+        <option value="4">Stuff</option>
+      </select>
     </form>
   )
 

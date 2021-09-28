b3e6cb14133e402f9171f705251a395c064efc86
Anthony Kim <fbssaejun@gmail.com>
2021-09-27 22:00:49 -0400

    combine all band routes into one file

--- server/routes/bands.js
+++ server/routes/bands.js
@@ -1,6 +1,15 @@
 const router = require('express').Router();
 
 module.exports = (db) => {
+
+  router.get('/leader-bands/:id', (req, res) => {
+    const query = `SELECT * FROM bands WHERE bands.leader_id = $1;`
+    db.query(query, [req.params.id])
+      .then((results) => {
+        res.json(results.rows);
+      })
+  });
+
   router.post('/new', (req, res) => {
     const spot = req.body.spotData;
     const length = spot.title.length;
@@ -29,9 +38,7 @@ module.exports = (db) => {
       ${spotValues.substring(0, spotValues.length - 2)}
       RETURNING band_id;
       `
-      console.log("SPOT QUERY:", spotQuery)
       db.query(spotQuery).then((results)=>{
-        console.log("AFTER SPOT QUERY:", results)
         return res.status(200).send({
         message: 'data inserted',
         result: results.rows[0]
@@ -40,5 +47,34 @@ module.exports = (db) => {
     });
   });
 
+  router.delete('/:id', (req, res) => {
+    const bandId = req.params.id;
+    const query = `DELETE FROM bands WHERE bands.id = $1 RETURNING *;`;
+
+    db.query(query, [bandId]).then((results) => {
+      return res.status(200).send({
+        message: `band ${bandId} removed`,
+        result: results
+      });
+    })
+  })
+
+  router.get('/:id', (req, res) => {
+    const query = `SELECT * FROM bands WHERE id = $1;`
+    db.query(query, [req.params.id]
+      
+      ).then((results) => {
+        res.json(results.rows[0]);
+      });
+    });
+    
+    
+  router.get('/', (req, res) => {
+    const query = `SELECT * FROM bands;`
+    db.query(query).then((results) => {
+      res.json(results.rows);
+    });
+  });
+  
   return router;
 };

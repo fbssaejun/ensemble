54c309f357067eb66d01989b8279655d95467586
KangerDrew <kangdrew95@gmail.com>
2021-09-28 22:52:31 -0700

    Case sensitivity fixed

--- server/routes/search.js
+++ server/routes/search.js
@@ -11,15 +11,15 @@ module.exports = (db) => {
     const query_users =`
       SELECT genre_id, NULL AS instrument_id, users.* FROM user_genre
       LEFT JOIN users ON users.id = user_genre.user_id
-      WHERE LOWER(users.first_name) LIKE $1
-      OR LOWER(users.last_name) LIKE $1
-      OR LOWER(users.username) LIKE $1
+      WHERE LOWER(users.first_name) LIKE LOWER($1)
+      OR LOWER(users.last_name) LIKE LOWER($1)
+      OR LOWER(users.username) LIKE LOWER($1)
       UNION
       SELECT NULL AS genre_id, instrument_id, users.* FROM user_instrument
       LEFT JOIN users ON users.id = user_instrument.user_id
-      WHERE LOWER(users.first_name) LIKE $1
-      OR LOWER(users.last_name) LIKE $1
-      OR LOWER(users.username) LIKE $1;
+      WHERE LOWER(users.first_name) LIKE LOWER($1)
+      OR LOWER(users.last_name) LIKE LOWER($1)
+      OR LOWER(users.username) LIKE LOWER($1);
     `;
     
     // to find bands when term is user name, add band leader name to band schema, also update for createband
@@ -27,8 +27,8 @@ module.exports = (db) => {
       SELECT genre_id, bands.*, spots.instrument_id AS spot_instrument, spots.id AS spot_id, spots.user_id AS spot_user_id, spots.title AS spot_title, spots.description AS spot_description FROM band_genre
       LEFT JOIN bands ON bands.id = band_genre.band_id
       LEFT JOIN spots ON bands.id = spots.band_id
-      WHERE LOWER(bands.name) LIKE $1
-      OR LOWER(bands.description) LIKE $1;
+      WHERE LOWER(bands.name) LIKE LOWER($1)
+      OR LOWER(bands.description) LIKE LOWER($1);
     `;
 
     Promise.all([
@@ -79,8 +79,8 @@ TESTING TERMS
 SELECT genre_id, bands.*, spots.instrument_id AS spot_instrument, spots.id AS spot_id, spots.user_id AS spot_user_id, spots.title AS spot_title, spots.description AS spot_description FROM band_genre
       LEFT JOIN bands ON bands.id = band_genre.band_id
       LEFT JOIN spots ON bands.id = spots.band_id
-      WHERE LOWER(bands.name) LIKE '%pol%'
-      OR LOWER(bands.description) LIKE '%pol%';
+      WHERE LOWER(bands.name) LIKE LOWER('%PO%')
+      OR LOWER(bands.description) LIKE LOWER('%PO%');
 
 */
 

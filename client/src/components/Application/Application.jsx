33dc80f9442f5fa851b36cf93ee7621ca5a4b83e
Anthony Kim <fbssaejun@gmail.com>
2021-09-29 22:33:32 -0400

    add component to render user's applications

--- client/src/components/Application/Application.jsx
+++ client/src/components/Application/Application.jsx
@@ -1,8 +1,18 @@
 
 export default function Application (props) {
+  const { bandName, description, title, instrument, message, acceptedStatus } = props;
 
   return (
-
+    <div>
+      <h1>{bandName}</h1>
+      <h2>{title}</h2>
+      <h3>{instrument}</h3>
+      <h3>{message}</h3>
+      <h3>{description}</h3>
+      <h4>{acceptedStatus}</h4>
+    </div>
   );
 
 }
+
+
--- client/src/components/Application/ApplicationList.jsx
+++ client/src/components/Application/ApplicationList.jsx
@@ -1,14 +1,35 @@
 import axios from "axios";
 import Application from "./Application";
+import { useState, useEffect, } from 'react';
+import { useParams } from 'react-router-dom';
 
-export default function ApplicationList () {
-
+export default function ApplicationList (props) {
+  const [applications, setApplications] = useState([]);
+  const { userId } = useParams();
+  console.log("whats userId:", userId)
   useEffect(() => {
-    axios.get(`/api/applications/${userId}`)
+    axios.get(`/api/applications/${userId}`).then((results) => {
+      console.log("application get back", results);
+      setApplications(() => [...results.data]);
+    })
 
   }, [])
 
+  const applicationArr = applications.map((application) => {
+    return <Application 
+      key={application.id}
+      bandName={application.band_name} 
+      description={application.description}
+      title={application.title}
+      instrument={application.instrument}
+      message={application.message}
+      acceptedStatus={application.accepted_status}
+    />
+  })
+
   return (
-    
+    <div>
+      {applicationArr}
+    </div>
   );
 }

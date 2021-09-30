60e43cce2db9bb989dc7f5f755f0a5f707d92cc6
KangerDrew <kangdrew95@gmail.com>
2021-09-30 10:31:33 -0700

    Autocomplete is working. Need to refactor axios promises and have the user instruments display.

--- client/src/components/UserEdit.jsx
+++ client/src/components/UserEdit.jsx
@@ -1,29 +1,59 @@
 import axios from 'axios';
 import { useEffect, useState, Fragment } from 'react';
 import { useParams } from 'react-router-dom'
-
+import Autocomplete from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
 
 export default function Navbar(props) {
   const { userId } = useParams();
   const [userInst, setUserInst] = useState();
   const [userGenre, setUserGenre] = useState();
+  const [allInst, setAllInst] = useState();
 
   useEffect(() => {
 
-    axios.get(`/api/users/${userId}/edit`)
+
+    Promise.all([
+      axios.get(`/api/users/${userId}/edit`),
+      axios.get('/api/instruments'),
+      axios.get('/api/genres')
+    ])
+    
     .then(results => {
       setUserInst([...results.data.instResult])
       setUserGenre([...results.data.genreResult])
     })
 
+    
+    .then(results => {
+      setAllInst([...results.data])
+    })
+
 
   },[]);
+
 
   return (
     <Fragment>
       <h1> Abot You </h1>
       <h3>{JSON.stringify(userInst)}</h3>
       <h3>{JSON.stringify(userGenre)}</h3>
+      {userInst && <Autocomplete
+        multiple
+        id="tags-outlined"
+        options={allInst}
+        getOptionLabel={(option) => option.name}
+        defaultValue={[...userInst]}
+        filterSelectedOptions
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            label="Your Instruments"
+            placeholder="What Do You Play?"
+          />
+        )}
+      />}
     </Fragment>
 
   );

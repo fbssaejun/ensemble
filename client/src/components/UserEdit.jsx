d29091429b518caf0888389ae80f051b2675a0fd
KangerDrew <kangdrew95@gmail.com>
2021-09-30 10:20:29 -0700

    About page now displays user instruments and genres

--- client/src/components/UserEdit.jsx
+++ client/src/components/UserEdit.jsx
@@ -1,16 +1,30 @@
-import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useEffect, useState, Fragment } from 'react';
+import { useParams } from 'react-router-dom'
 
 
 export default function Navbar(props) {
-
+  const { userId } = useParams();
+  const [userInst, setUserInst] = useState();
+  const [userGenre, setUserGenre] = useState();
 
   useEffect(() => {
+
+    axios.get(`/api/users/${userId}/edit`)
+    .then(results => {
+      setUserInst([...results.data.instResult])
+      setUserGenre([...results.data.genreResult])
+    })
 
 
   },[]);
 
   return (
-    <h1> Abot You </h1>
+    <Fragment>
+      <h1> Abot You </h1>
+      <h3>{JSON.stringify(userInst)}</h3>
+      <h3>{JSON.stringify(userGenre)}</h3>
+    </Fragment>
 
   );
 

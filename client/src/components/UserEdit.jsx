238c87be7cb299237b922032816954bfdcee2855
KangerDrew <kangdrew95@gmail.com>
2021-09-30 12:21:51 -0700

    Now receiving values form Autocomplete component. Need to set up backend to receive this info.

--- client/src/components/UserEdit.jsx
+++ client/src/components/UserEdit.jsx
@@ -3,14 +3,25 @@ import { useEffect, useState, Fragment } from 'react';
 import { useParams } from 'react-router-dom'
 import Autocomplete from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
+import Stack from '@mui/material/Stack';
+
 
 export default function Navbar(props) {
   const { userId } = useParams();
+
+  // defaultInst and defaultGenre is to get initial user
+  // genre and instrument info
+  const [defaultInst, setDefaultInst] = useState();
+  const [defaultGenre, setDefaultGenre] = useState();
+
+  // userInst and userGenre are to obtain the input information
+  // from Autocomplete component
   const [userInst, setUserInst] = useState();
   const [userGenre, setUserGenre] = useState();
   const [allInst, setAllInst] = useState();
   const [allGenre, setAllGenre] = useState();
 
+  
   useEffect(() => {
 
     Promise.all([
@@ -23,14 +34,15 @@ export default function Navbar(props) {
       const inst = all[1].data;
       const genre = all[2].data;
 
-    
       setUserInst([...userInfo.instResult]);
       setUserGenre([...userInfo.genreResult]);
 
-      // Don't need to convert inst or genre because Autocomplete component
-      // from MUI takes objects for their options.
+      setDefaultInst([...userInfo.instResult]);
+      setDefaultGenre([...userInfo.genreResult]);
+
       setAllInst([...inst]);
       setAllGenre([...genre]);
+
 
     })
 
@@ -60,44 +72,56 @@ export default function Navbar(props) {
 
   };
 
+  const submitOptions = (event) => {
+    event.preventDefault();
+    console.log(userInst);
+    console.log(userGenre);
+
+  }
+
   return (
     <Fragment>
       <h1> Abot You </h1>
-      <h3>{JSON.stringify(userInst)}</h3>
-      <h5>{JSON.stringify(allInst)}</h5>
-
-      <h3>{JSON.stringify(userGenre)}</h3>
-      <h5>{JSON.stringify(allGenre)}</h5>
-      {allInst && <Autocomplete
-        multiple
-        id="tags-outlined"
-        options={allInst}
-        getOptionLabel={(option) => option.name}
-        defaultValue={preSelected(userInst, allInst)}
-        Instruments
-        renderInput={(params) => (
-          <TextField
-            {...params}
-            label="Instruments"
-            placeholder="Add More Instruments"
-          />
-        )}
-      />} <br />
-      {allGenre && <Autocomplete
-        multiple
-        id="tags-outlined"
-        options={allGenre}
-        getOptionLabel={(option) => option.name}
-        defaultValue={preSelected(userGenre, allGenre)}
-        Instruments
-        renderInput={(params) => (
-          <TextField
-            {...params}
-            label="Instruments"
-            placeholder="Add More Instruments"
-          />
-        )}
-      />}
+      <br />
+        <form onSubmit={submitOptions}>
+          <Stack spacing={2} sx={{ width: 500 }}>
+          <h3> Your Instruments </h3>
+          {allInst && <Autocomplete
+            multiple
+            onChange={(event, value) => setUserInst(value)}
+            id="tags-outlined"
+            options={allInst}
+            getOptionLabel={(option) => option.name}
+            defaultValue={preSelected(defaultInst, allInst)}
+            Instruments
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Instruments"
+                placeholder="Add More Instruments"
+              />
+            )}
+          />} 
+          <h3> Your Genres </h3>
+          {allGenre && <Autocomplete
+            multiple
+            onChange={(event, value) => setUserGenre(value)}
+            id="tags-outlined"
+            options={allGenre}
+            getOptionLabel={(option) => option.name}
+            defaultValue={preSelected(defaultGenre, allGenre)}
+            Genres
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Genres"
+                placeholder="Add More Genres"
+              />
+            )}
+          />}
+          </Stack>
+          <button>Submit Me!</button>
+        </form>
     </Fragment>
 
   );

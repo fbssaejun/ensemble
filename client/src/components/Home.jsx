1a2fc0d43b4a32a68a8a4cf90e28f58d4b65710a
Anthony Kim <fbssaejun@gmail.com>
2021-10-01 16:33:32 -0400

    add content boxes in main section

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,51 +1,70 @@
-import { Fragment } from 'react';
+import { requirePropFactory } from "@mui/material";
+import { Fragment } from "react";
 import ReactPlayer from "react-player";
-export default function Home () {
-  
+import "./Home.scss";
+import trumpet from './trumpet.jpeg'
+
+export default function Home() {
   return (
-    <Fragment>
-
+    <Fragment className="home-component">
       <div className="vimeo-video">
-        <video width="100%" height="500" controls >
+        <ReactPlayer 
+        url='https://vimeo.com/619592171/e80bf00b7f' 
+        playing="true"
+        loop="true"
+        volume="0"
+        width="100%"
+        />
+         {/* <video width="100%" height="500" controls >
           <source src="../../public/home-video.mp4" type="video/mp4"/>
-        </video>
+        </video>  */}
       </div>
 
 
       <div>
-        <div >
-          hello
+        <h2>Featured Bands</h2>
+      </div>
+
+
+      <div className="home-container">
+        <div className="picture">
+          <img src={trumpet} alt="trumpet"/>
         </div>
-        <div>
-          hello
+        <div className="content">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer scelerisque elit quis diam venenatis, ac auctor metus lobortis. Ut posuere porttitor velit vel condimentum. Sed ac arcu vitae elit porta pulvinar nec non eros. Curabitur efficitur lacus sed interdum ultricies. Phasellus suscipit varius tortor, a ullamcorper libero tempor quis. 
         </div>
       </div>
 
 
-      <div>
-        <h2>Content2</h2>
+      <div className="home-container">
+        <div className="content">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer scelerisque elit quis diam venenatis, ac auctor metus lobortis. Ut posuere porttitor velit vel condimentum. Sed ac arcu vitae elit porta pulvinar nec non eros. Curabitur efficitur lacus sed interdum ultricies. Phasellus suscipit varius tortor, a ullamcorper libero tempor quis. 
+        </div>
+        <div className="picture">
+          picture
+        </div>
       </div>
 
 
-      <div>
-        <h2>Content3</h2>
+      <div className="home-container">
+        <div className="picture">
+          picture
+        </div>
+        <div className="content">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer scelerisque elit quis diam venenatis, ac auctor metus lobortis. Ut posuere porttitor velit vel condimentum. Sed ac arcu vitae elit porta pulvinar nec non eros. Curabitur efficitur lacus sed interdum ultricies. Phasellus suscipit varius tortor, a ullamcorper libero tempor quis. 
+        </div>
       </div>
 
 
-      <div>
-        <h2>Content4</h2>
+      <div className="home-container">
+        <div className="content">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer scelerisque elit quis diam venenatis, ac auctor metus lobortis. Ut posuere porttitor velit vel condimentum. Sed ac arcu vitae elit porta pulvinar nec non eros. Curabitur efficitur lacus sed interdum ultricies. Phasellus suscipit varius tortor, a ullamcorper libero tempor quis. 
+        </div>
+        <div className="picture">
+          picture
+        </div>
       </div>
-
-
-      <div>
-        <h2>Slideshow?</h2>
-      </div>
-
-
-      <footer>
-        <h1>Footer</h1>
-      </footer>
 
     </Fragment>
-  )
+  );
 }

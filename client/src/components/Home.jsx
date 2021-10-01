eaeeb958545ec732273576cf3967166b051a4a5b
Anthony Kim <fbssaejun@gmail.com>
2021-10-01 13:23:41 -0400

    add video playout

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,37 +1,51 @@
-import ReactPlayer from 'react-player';
 import { Fragment } from 'react';
-
+import ReactPlayer from "react-player";
 export default function Home () {
-
+  
   return (
     <Fragment>
-      <h1>Blank Homepage</h1>
+
       <div className="vimeo-video">
-        <ReactPlayer 
-        url="https://vimeo.com/616081425/f02e50a688" 
-        playing
-        controls
-        volume="0"
-        loop />
+        <video width="100%" height="500" controls >
+          <source src="../../public/home-video.mp4" type="video/mp4"/>
+        </video>
       </div>
+
+
       <div>
-        <h2>Content1</h2>
+        <div >
+          hello
+        </div>
+        <div>
+          hello
+        </div>
       </div>
+
+
       <div>
         <h2>Content2</h2>
       </div>
+
+
       <div>
         <h2>Content3</h2>
       </div>
+
+
       <div>
         <h2>Content4</h2>
       </div>
+
+
       <div>
         <h2>Slideshow?</h2>
       </div>
+
+
       <footer>
         <h1>Footer</h1>
       </footer>
+
     </Fragment>
   )
 }

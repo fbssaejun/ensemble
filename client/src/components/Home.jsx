1b55015588bed57c8367f5d4bb2ed52733aac755
Anthony Kim <fbssaejun@gmail.com>
2021-10-01 19:34:05 -0400

    redirect users to the band's page when they click on specific featured band

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,70 +1,155 @@
-import { requirePropFactory } from "@mui/material";
-import { Fragment } from "react";
-import ReactPlayer from "react-player";
+import { Fragment, useEffect, useState, useRef } from "react";
 import "./Home.scss";
-import trumpet from './trumpet.jpeg'
+import trumpet from "./trumpet.jpeg";
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
+
+// import ReactPlayer from "react-player";
+// import homeVideo from "./home-video.mp4";
 
 export default function Home() {
+
+  // const colors = ["Oasis", "Guns N roses"];
+  const delay = 2500;
+
+  const [featuredBands, setFeaturedBands] = useState([])
+  const [index, setIndex] = useState(0);
+  const timeoutRef = useRef(null);
+  const history = useHistory();
+
+
+  const resetTimeout = () => {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+  }
+
+  useEffect(() => {
+
+
+    axios.get('/api/bands/featured').then((results) => {
+      setFeaturedBands(results.data)
+    });
+
+    resetTimeout();
+    timeoutRef.current = setTimeout(
+      () =>
+        setIndex((prevIndex) =>
+          prevIndex === featuredBands.length - 1 ? 0 : prevIndex + 1
+        ),
+      delay
+    );
+
+    return () => {
+      resetTimeout();
+    };
+  }, [index]);
+
   return (
     <Fragment className="home-component">
-      <div className="vimeo-video">
-        <ReactPlayer 
-        url='https://vimeo.com/619592171/e80bf00b7f' 
-        playing="true"
-        loop="true"
-        volume="0"
-        width="100%"
-        />
-         {/* <video width="100%" height="500" controls >
-          <source src="../../public/home-video.mp4" type="video/mp4"/>
-        </video>  */}
-      </div>
+      {/* <div className="vimeo-video">
+   
+         <video width="100%" height="500" controls autoplay>
+          <source src={homeVideo} type="video/mp4"/>
+        </video> 
+      </div> */}
 
-
-      <div>
+      <div className="main">
         <h2>Featured Bands</h2>
+        <div className="slideshow">
+          <div
+            className="slideshowSlider"
+            style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}
+          >
+            {featuredBands.map((bands, index) => (
+              <button
+                className="slide"
+                key={index}        
+                onClick={() => {
+                  history.push(`/bands/${bands.id}`)
+                }}         
+              >
+                <h2>{bands.name}</h2>
+                <h3>{bands.description}</h3>
+              </button>
+            ))}
+          </div>
+
+          <div className="slideshowDots">
+            {featuredBands.map((_, idx) => (
+              <div
+                key={idx}
+                className={`slideshowDot${index === idx ? " active" : ""}`}
+                onClick={() => {
+                  setIndex(idx);
+                }}
+              ></div>
+            ))}
+          </div>
+        </div>
+        <div className="explore-text">
+          <h6>Looking for a band?</h6>
+          <p><b>Explore and join according to your favorite genre, instrument and people!</b></p>
+        </div>
       </div>
 
+      <div className="home-container">
+
+
+        <div className="picture">
+          <img src={trumpet} alt="trumpet" width="200em;" />
+        </div>
+        <div className="content">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+          scelerisque elit quis diam venenatis, ac auctor metus lobortis. Ut
+          posuere porttitor velit vel condimentum. Sed ac arcu vitae elit porta
+          pulvinar nec non eros. Curabitur efficitur lacus sed interdum
+          ultricies. Phasellus suscipit varius tortor, a ullamcorper libero
+          tempor quis.
+        </div>
+      </div>
+
+      <div className="home-container">
+        <div className="content">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+          scelerisque elit quis diam venenatis, ac auctor metus lobortis. Ut
+          posuere porttitor velit vel condimentum. Sed ac arcu vitae elit porta
+          pulvinar nec non eros. Curabitur efficitur lacus sed interdum
+          ultricies. Phasellus suscipit varius tortor, a ullamcorper libero
+          tempor quis.
+        </div>
+        <div className="picture">
+          <img src={trumpet} alt="trumpet" width="200em;" />
+        </div>
+      </div>
 
       <div className="home-container">
         <div className="picture">
-          <img src={trumpet} alt="trumpet"/>
+          <img src={trumpet} alt="trumpet" width="200em;" />
         </div>
         <div className="content">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer scelerisque elit quis diam venenatis, ac auctor metus lobortis. Ut posuere porttitor velit vel condimentum. Sed ac arcu vitae elit porta pulvinar nec non eros. Curabitur efficitur lacus sed interdum ultricies. Phasellus suscipit varius tortor, a ullamcorper libero tempor quis. 
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+          scelerisque elit quis diam venenatis, ac auctor metus lobortis. Ut
+          posuere porttitor velit vel condimentum. Sed ac arcu vitae elit porta
+          pulvinar nec non eros. Curabitur efficitur lacus sed interdum
+          ultricies. Phasellus suscipit varius tortor, a ullamcorper libero
+          tempor quis.
         </div>
       </div>
-
 
       <div className="home-container">
         <div className="content">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer scelerisque elit quis diam venenatis, ac auctor metus lobortis. Ut posuere porttitor velit vel condimentum. Sed ac arcu vitae elit porta pulvinar nec non eros. Curabitur efficitur lacus sed interdum ultricies. Phasellus suscipit varius tortor, a ullamcorper libero tempor quis. 
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+          scelerisque elit quis diam venenatis, ac auctor metus lobortis. Ut
+          posuere porttitor velit vel condimentum. Sed ac arcu vitae elit porta
+          pulvinar nec non eros. Curabitur efficitur lacus sed interdum
+          ultricies. Phasellus suscipit varius tortor, a ullamcorper libero
+          tempor quis.
         </div>
         <div className="picture">
-          picture
+          <img src={trumpet} alt="trumpet" width="200em;" />
         </div>
       </div>
-
-
-      <div className="home-container">
-        <div className="picture">
-          picture
-        </div>
-        <div className="content">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer scelerisque elit quis diam venenatis, ac auctor metus lobortis. Ut posuere porttitor velit vel condimentum. Sed ac arcu vitae elit porta pulvinar nec non eros. Curabitur efficitur lacus sed interdum ultricies. Phasellus suscipit varius tortor, a ullamcorper libero tempor quis. 
-        </div>
-      </div>
-
-
-      <div className="home-container">
-        <div className="content">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer scelerisque elit quis diam venenatis, ac auctor metus lobortis. Ut posuere porttitor velit vel condimentum. Sed ac arcu vitae elit porta pulvinar nec non eros. Curabitur efficitur lacus sed interdum ultricies. Phasellus suscipit varius tortor, a ullamcorper libero tempor quis. 
-        </div>
-        <div className="picture">
-          picture
-        </div>
-      </div>
-
     </Fragment>
   );
 }

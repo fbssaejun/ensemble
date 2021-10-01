c21e6c7999a19c0cb06d5fd240158188cf39e250
Anthony Kim <fbssaejun@gmail.com>
2021-10-01 16:32:32 -0400

    add footer component

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+
+
+export default function Footer() {
+  return (
+    <Fragment>
+    <footer>
+    <h1>This is Footer</h1>
+    </footer> 
+    </Fragment>
+  );
+};

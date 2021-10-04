0aceff9845dcedc2311eb737fbbee111a790fde7
Anthony Kim <fbssaejun@gmail.com>
2021-10-03 21:43:42 -0400

    change layout of footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -5,16 +5,25 @@ export default function Footer() {
   return (
     <Fragment>
     <footer>
-      <div>
+
+       <div clssName="contributors">
         <h4>Anthony Kim</h4>
+        <a>Github</a>
+        <a href="https://github.com/fbssaejun"><i class="fab fa-github"></i></a>
+       </div>
+
+       <div clssName="contributors">
         <h4>Drew Kang</h4>
+        <a>Github</a>
+        <a href="https://github.com/KangerDrew"><i class="fab fa-github"></i></a>
+       </div>
+
+       <div clssName="contributors">
         <h4>Tim Zheng</h4>
-      </div>
-      <div className="hello">
-        <h5><a href="https://github.com/fbssaejun"><i class="fab fa-github"></i></a></h5>
-        <h5><a href="https://github.com/KangerDrew"><i class="fab fa-github"></i></a></h5>
-        <h5><a href="https://github.com/Tzheng456"><i class="fab fa-github"></i></a></h5>
-      </div>
+        <a>Github</a>
+        <a href="https://github.com/Tzheng456"><i class="fab fa-github"></i></a>
+       </div>
+
     </footer> 
     </Fragment>
   );

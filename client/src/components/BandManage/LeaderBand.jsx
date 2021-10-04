87ba0aeb759d2ea7a288e8b58e5df8471ae86d18
KangerDrew <kangdrew95@gmail.com>
2021-10-04 13:06:08 -0700

    Added image to the top of the leader band card

--- client/src/components/BandManage/LeaderBand.jsx
+++ client/src/components/BandManage/LeaderBand.jsx
@@ -66,6 +66,9 @@ export default function LeaderBand(props) {
           </div>
         </Modal>
       </div>
+      <div className="leader-band-image">
+        <img src={image ? image : "https://media.istockphoto.com/vectors/fiddler-vector-id452097777?k=20&m=452097777&s=612x612&w=0&h=tEiPg3SQNeclfnvT24V7NFusHxe0291xOKZPBg1Sq-8="}/>
+      </div>
       <div className="spot-list-accordion">
         <Accordion className="leader-band-accordian">
           <AccordionSummary

f1c73d24f62895ac999bf7c53adc0452e8fbce5c
Anthony Kim <fbssaejun@gmail.com>
2021-10-04 01:57:11 -0400

    edit layout & styling for navbar

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Navbar.scss";
 import { Link } from "react-router-dom";
+import { Fragment } from 'react';
 import Dashboard from "./Dashboard";
 import logo from "./ensemble.png";
 
@@ -18,25 +19,30 @@ export default function Navbar(props) {
             alt="logo"
           />
         </Link>
-        <div className="search-bar">
-          <Link to="/search">Begin Your Search</Link>
-        </div>
           <div class="theme-toggle">
             <input type="checkbox" />
               <div class="toggle-body"></div>
               <div class="celestial-body"></div>
           </div>
         {!props.currentUser ? (
+          <Fragment>
+          <div className="search-bar">
+            <Link to="/search">Search Bands</Link>
+          </div>
           <div className="user">
             <Link to="/auth">Get Started</Link>
           </div>
+          </Fragment>
         ) : (
-          <React.Fragment>
+          <div className="search-and-icon">
+            <div className="search-bar">
+              <Link to="/search">Search</Link>
+            </div>
             <Dashboard
               currentUser={props.currentUser}
               setState={props.setState}
             />
-          </React.Fragment>
+            </div>
         )}
       </nav>
     </header>

7605052b4f947a6eafbc668a7572ab34a3d71b96
KangerDrew <kangdrew95@gmail.com>
2021-10-04 16:40:31 -0700

    user search result image is no longer using Slash photo

--- client/src/components/Result/UserResultList/UserResult.jsx
+++ client/src/components/Result/UserResultList/UserResult.jsx
@@ -7,12 +7,13 @@ import Typography from '@mui/material/Typography';
 import CardActionArea from '@mui/material/CardActionArea'
 
 export default function UserResult(props) {
-  const testImage = 'https://upload.wikimedia.org/wikipedia/commons/3/3d/Slash%2C_Guitarist_of_Guns_N%27_Roses_in_2017.jpg';
+
+  
   return (
       <Card className="single-user-result" sx={{ maxWidth: 300 }}>
          <CardActionArea component={Link}  to={`/users/${props.userId}`} >
           <CardContent>
-            <Avatar alt={props.firstName} src={props.profile_image ? props.profile_image : testImage} sx={{ width: 70, height: 70 }} />
+            <Avatar alt={props.firstName} src={props.profile_image ? props.profile_image : "/"} sx={{ width: 70, height: 70 }} />
             <Typography gutterBottom variant="h5" component="div">
               {props.firstName} {props.lastName}
             </Typography>
